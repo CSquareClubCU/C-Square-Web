9f562921704cd25a7e7100162bfcbce169af19a2
Karan Juneja <166711731+KJ1201@users.noreply.github.com>
2026-07-15 13:42:33 +0530

    Add cusoc redirect (#22)
    * Allow setting passwords for admin login
    
    * Fix duplicate index bug in event slug migration
    
    * Fix manager to enforce unusable passwords for magic links
    
    * Add redirect to /cusoc
    
    ---------
    
    Co-authored-by: CSquareClub <csquareclub@gmail.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  async redirects() {
+    return [
+      {
+        source: '/cusoc',
+        destination: 'https://cusoc.csquareclub.co.in/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
