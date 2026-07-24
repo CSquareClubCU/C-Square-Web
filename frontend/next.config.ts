a17222ad1aa80f0d216adc882f61f511b547bb75
Pratik-kr21 <pratik792584@gmail.com>
2026-07-24 15:49:55 +0530

    feat: setup progressive web app with serwist

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,10 @@
 import type { NextConfig } from "next";
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
 
 const nextConfig: NextConfig = {
   images: {
@@ -28,4 +34,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
